742b8f33606d69c3a775e695b80e5e6e10a2b309
Pawel Grzgorzewski <rudi_77@wp.pl>
2016-09-18 22:33:40 +0200

    rsa encryption works, css to improve, decryption to add

--- praca_inzynierska/rsa_data.js
+++ praca_inzynierska/rsa_data.js
@@ -6,12 +6,20 @@ var keyLenght;
 
 var publicKey;
 var publicKeyString;
-//var MattsRSAkey = cryptico.generateRSAKey(PassPhrase, Bits);
-//var MattsPublicKeyString = cryptico.publicKeyString(MattsRSAkey); 
+
+var encryptedMessageText = '';
+//var EncryptionResult = cryptico.encrypt(PlainText, MattsPublicKeyString);
+
+// Flags 
+var keyPharseSet = 0;
+var messageSet = 0;
+var keySet = 0;
+
 
 function setMessage()		//zapisywanie wiadomości
 {
 	messageText = document.getElementById('message').value;
+	messageSet = 1;
 }
 
 function setKeyLenght()
@@ -23,13 +31,46 @@ function setKeyLenght()
 function setKeyPharse()		//zapisywanie klucza
 {
 	keyPharseKeyText  = document.getElementById('Key_pharse').value;
-	
+	keyPharseSet = 1;
 }
 
 function setKeyText() 		//wpisanie klucza do pola tekstowego
 {
-	publicKey = cryptico.generateRSAKey(messageText, keyLenght);
-	publicKeyString = cryptico.publicKeyString(publicKey);
-	document.getElementById('keyText').value = publicKeyString;
+	if(keyPharseSet == 1)
+	{	
+		publicKey = cryptico.generateRSAKey(messageText, keyLenght);
+		publicKeyString = cryptico.publicKeyString(publicKey);
+		document.getElementById('keyText').value = publicKeyString;
+		keySet = 1;		
+	}
+	else
+	{
+		alert("Please put in you key pharse");
+	}
+}
+
+function encrypt()
+{
+	if(messageSet == 1 && keySet == 1)
+	{
+		encryptedMessageText = cryptico.encrypt(messageText, publicKeyString);
+		document.getElementById('encryptedText').value = encryptedMessageText.cipher;
+	}
+	else if(messageSet == 0 && keySet == 1)
+	{
+		alert("please write the message You would like to encrypt");
+	}
+	else if(messageSet == 1 && keySet == 0)
+	{
+		alert("plaese generate key");
+	}
+	else
+	{
+		alert("please generate key and write the message You would like to encrypt");
+	}
+}
+
+function showSafety()
+{
 	
 }
